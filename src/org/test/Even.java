147c545a5ff029feb754c46f29812edee67cfbaa
maheshreddy97 <maheshreddycr7@gmail.com>
2019-09-12 18:00:06 +0530

    added even

---
+++ src/org/test/Even.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Even {
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i = 1; i <=100; i++) {
+			if(i%2==0) {
+				sum=sum+i;
+			}
+			
+		}
+		System.out.println(sum);
+	}
+
+}
